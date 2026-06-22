5c3b6687681d76893aa0b8ec359be6ff27d8e4a7
SAMUEL VICTOR ASBAHR MACIEL <samuel.maciel5@senaisp.edu.br>
2026-06-22 10:59:47 -0300

    Correção prof

--- src/controllers/imagemProduto.controller.js
+++ src/controllers/imagemProduto.controller.js
@@ -1,35 +1,43 @@
 import path from "path";
-import { fileURLToPath } from "url";
 import fs from "fs";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const imagemProduto = {
   exibirImagem: async (req, res) => {
     try {
       const { nome } = req.params;
-      const caminhoAbsoluto = path.join(
-        __dirname,
-        "../../uploads/images/",
+
+      const caminhoImagem = path.join(
+        process.cwd(),
+        "uploads",
+        "images",
         nome
       );
 
-      if (!fs.existsSync(caminhoAbsoluto)) {
-        return res.status(404).json({ message: "Imagem nao encontrada" });
+      console.log(caminhoImagem);
+
+      if (!fs.existsSync(caminhoImagem)) {
+        return res.status(404).json({
+          message: "Imagem não encontrada",
+        });
       }
 
-      return res.sendFile(caminhoAbsoluto, err => {
+      return res.sendFile(caminhoImagem, (err) => {
         if (err) {
-          console.error('sendFile error:', err);
+          console.error("sendFile error:", err);
+
           if (!res.headersSent) {
-            return res.status(500).json({ message: "Erro ao enviar imagem" });
+            return res.status(500).json({
+              message: "Erro ao enviar imagem",
+            });
           }
         }
       });
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: "Ocorreu um erro no servidor" });
+
+      return res.status(500).json({
+        message: "Ocorreu um erro no servidor",
+      });
     }
   },
 };
