f2a68539c98af7fd0d32878dd629e8b53c3a9ebf
SAMUEL VICTOR ASBAHR MACIEL <samuel.maciel5@senaisp.edu.br>
2026-06-22 09:23:41 -0300

    adicionando imagemProdutoController

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import produtoRoutes from "./produtosRoutes.js";
 import categoriaRoutes from "./categoriaRoutes.js";
 import pedidoRoutes from "./pedidoRoutes.js";
+import imagemProdutoRoutes from "./imagemProduto.routes.js";
 
 const routes = Router();
 
@@ -9,6 +10,7 @@ routes.use("/produtos", produtoRoutes);
 routes.use("/categorias", categoriaRoutes);
 routes.use("/pedidos", pedidoRoutes);
 routes.use("/itens", pedidoRoutes);
+routes.use("/images", imagemProdutoRoutes);
 
 
 export default routes;
