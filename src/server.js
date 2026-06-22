13cae6f9194f8c89f2210d991c7fa5d354fd32a8
SAMUEL VICTOR ASBAHR MACIEL <samuel.maciel5@senaisp.edu.br>
2026-06-22 10:25:41 -0300

    arrumando imagerout2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes/router.js";
+import cors from "cors";
 // import { initializeDatabase } from "./config/Database.js";
-let port = 8080;
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use("/", routes);
+
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(
